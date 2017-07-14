7220bd8bdff714883bfad3d2dd0de8b391e557a3
Zukune <esteban.019@hotmail.com>
2017-07-13 20:09:10 -0500

    commit

--- app/src/main/java/com/mangaschool_app/cardview/Slidershow.java
+++ app/src/main/java/com/mangaschool_app/cardview/Slidershow.java
@@ -10,6 +10,7 @@ import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.mangaschool_app.cardview.model.Album;
 
 import java.util.ArrayList;
 
@@ -28,18 +29,18 @@ public class Slidershow extends AppCompatActivity implements  ViewPagerEx.OnPage
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String album = extras.getString("");
+            int album = extras.getInt("album");
 
 
             switch (album) {
 
-                case "0":
+                case 0:
                     urlList.add("http://imageshack.com/a/img922/6719/xLjXwa.jpg");
                     urlList.add("http://imageshack.com/a/img922/6719/xLjXwa.jpg");
                     urlList.add("http://imageshack.com/a/img922/6719/xLjXwa.jpg");
                     urlList.add("http://imageshack.com/a/img922/6719/xLjXwa.jpg");
                     break;
-                case "1":
+                case 1:
                     urlList.add("http://imageshack.com/a/img923/6757/M70jrN.jpg");
                     urlList.add("http://imageshack.com/a/img923/6757/M70jrN.jpg");
                     urlList.add("http://imageshack.com/a/img923/6757/M70jrN.jpg");
@@ -47,21 +48,21 @@ public class Slidershow extends AppCompatActivity implements  ViewPagerEx.OnPage
 
                     break;
 
-                case "2":
+                case 2:
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpgg");
 
                     break;
-                case "3":
+                case 3:
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
                     urlList.add("http://imageshack.com/a/img922/2751/85KVWm.jpg");
 
                     break;
-                case "4":
+                case 4:
                     urlList.add("http://imageshack.com/a/img922/5769/WtMb5K.jpg");
                     urlList.add("http://imageshack.com/a/img922/5769/WtMb5K.jpg");
                     urlList.add("http://imageshack.com/a/img922/5769/WtMb5K.jpg");
